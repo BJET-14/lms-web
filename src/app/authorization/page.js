8033f055b15e4b100328aff74a1a4407b44a9b2d
Exoalam <nafiul.arpon@gmail.com>
2024-06-29 22:06:56 +0600

    update

--- src/app/authorization/page.js
+++ src/app/authorization/page.js
@@ -1,8 +1,15 @@
-import React from 'react'
+'use client'
+import React, {useEffect, useState} from 'react'
 
 const Authorization = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    useEffect(() => {
+        console.log('Authorization page loaded')
+    }, [])
   return (
-    <div>Auth</div>
+    <div>
+        <button>Hello</button>
+    </div>
   )
 }
 
