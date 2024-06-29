2233b48a98a5dab7177a75d827c03193b8f323b1
Exoalam <nafiul.arpon@gmail.com>
2024-06-29 21:53:02 +0600

    update

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,21 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>Hello</div>
+    <div data-theme="light">
+      <div className="card lg:card-side bg-base-100 shadow-xl mx-auto w-1/2">
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg"
+            alt="Album" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">New album is released!</h2>
+          <p>Click the button to listen on Spotiwhy app.</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Listen</button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
